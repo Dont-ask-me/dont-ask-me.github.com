553bc8c2dcdd9470097ba73689ab0b8c6e24288e
Dont-ask-me <40747778+Dont-ask-me@users.noreply.github.com>
2021-03-05 18:21:53 +0800

    Add files via upload

---
+++ src/settings.0ef80.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/Game.fire",orientation:"portrait",jsList:[],bundleVers:{resources:"18179",internal:"b25de",main:"5d643"}};
